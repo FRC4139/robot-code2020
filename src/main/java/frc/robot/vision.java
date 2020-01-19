1e7d291ee313cd1b0deeca858e37c3bd0507d574
Luke-Desktop <lukekim1029@gmail.com>
2020-01-19 12:09:14 -0800

    Partially working vision code

--- src/main/java/frc/robot/vision.java
+++ src/main/java/frc/robot/vision.java
@@ -5,12 +5,20 @@ import org.opencv.core.Mat;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
+import java.awt.image.BufferedImage;
 
 public class vision{
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         VideoCapture camera = new VideoCapture(0);
         Mat frame = new Mat();
-        camera.read(frame);
+
+        while(true){
+            if (camera.read(frame)){
+                Imgcodecs.imwrite("feed.jpg", frame);
+                System.out.println("ok");
+                
+            }
+        }
     }
 }
