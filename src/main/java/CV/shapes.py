c5e369988ce598b8f66ec3b1b90d5be7e10f3154
pahels <pahel.srivastava@gmail.com>
2020-01-28 15:59:16 -0800

    shape detection

---
+++ src/main/java/CV/shapes.py
@@ -0,0 +1,17 @@
+import cv2
+
+class ShapeDetector:
+	def __init__(self):
+		pass
+
+		def detect (self, c):
+			#init shape name and approx. contour
+			shape = "unidentified"
+			peri = cv2.arcLength(c, True)
+			approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+
+			if len(approx) == 6:
+			shape = "hexagon"
+
+			return shape
