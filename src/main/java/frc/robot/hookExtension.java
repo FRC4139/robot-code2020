3bfff5e4ef7eadae2ac9e983602e5eba1476fa2e
Lagginggames <lagginggames@gmail.com>
2020-02-03 15:48:47 -0800

    fixed hook extension

--- src/main/java/frc/robot/hookExtension.java
+++ src/main/java/frc/robot/hookExtension.java
@@ -4,15 +4,23 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class hookExtension
 {
+    private WPI_TalonSRX motor;
+
+    //port 1 place holder
+    public hookExtension(int port1)
+     {
+        motor = new WPI_TalonSRX(port1);
+    }
+    
     //lowers hook
-    public void lower (WPI_TalonSRX drop)
+    public void lower()
     {
-            drop.set(-.75);
+            motor.set(-.75);
     }
 
     //raises hook
-    public void raise (WPI_TalonSRX lift)
+    public void raise()
     {
-        lift.set(.75);
+        motor.set(.75);
     }
 }
