c03c63aa9cde47785b0514a96b999883961cfd6d
Dylanlucas01 <58743412+Dylanlucas01@users.noreply.github.com>
2020-02-10 14:56:47 -0800

    updating shooter

--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -7,11 +7,7 @@ public class Shooter
     private WPI_TalonSRX shoot1;
     private WPI_TalonSRX shoot2;
 
-<<<<<<< HEAD:src/main/java/frc/robot/shooter.java
-    public shooter(int p1, int p2)
-=======
-    public Shooter(int p1)
->>>>>>> ee895075d220cd677a32b8d14769e8b226cd7c62:src/main/java/frc/robot/Shooter.java
+    public Shooter(int p1, int p2)
     {
         shoot1 = new WPI_TalonSRX(p1);
         shoot2 = new WPI_TalonSRX(p2);
