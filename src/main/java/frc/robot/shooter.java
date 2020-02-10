3e9aed8a075da53f66b9b83b30601a8e344a4231
Dylanlucas01 <58743412+Dylanlucas01@users.noreply.github.com>
2020-02-10 14:52:59 -0800

    updating shooter

--- src/main/java/frc/robot/shooter.java
+++ src/main/java/frc/robot/shooter.java
@@ -4,15 +4,18 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class shooter
 {
-    private WPI_TalonSRX shoot;
+    private WPI_TalonSRX shoot1;
+    private WPI_TalonSRX shoot2;
 
-    public shooter(int p1)
+    public shooter(int p1, int p2)
     {
-        shoot = new WPI_TalonSRX(p1);
+        shoot1 = new WPI_TalonSRX(p1);
+        shoot2 = new WPI_TalonSRX(p2);
     }
 
     public void shootRun(double val)
     {
-        shoot.set(val);
+        shoot1.set(val);
+        shoot2.set(-val);
     }
 }
