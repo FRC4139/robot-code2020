776de2ce66671f3059fde072192f2d2e6ffa948e
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2020-02-18 15:17:05 -0800

    Updated controller code

--- src/main/java/frc/robot/controller.java
+++ src/main/java/frc/robot/controller.java
@@ -7,16 +7,8 @@ public class Controller{
     //main controller
     //private XboxController controller1;
     private Wheels wheels;
-    //controller for color spinner
     private int intakeVal;
-    //private XboxController controller2;
-    private Conveyor conveyor;
-    private int intConvPort1, intConvPort2;
-    private double convDriveVal;
-    private double driveVal;
-    //private DrawBridge drawbridge;
-    private int drumPWMChannel;
-    private double drumVal;
+    //    CONVEYOR CODE DELETED
     private Intake intake;
     private int intakePort;
     private int wheelPort1, wheelPort2, wheelPort3, wheelPort4;
@@ -27,86 +19,81 @@ public class Controller{
     private ColorWheel colorWheel;
     private int colorPort;
     private double colorWheelVal;
-    //private ColorArm colorArm;
-    private int servoPWMChannel;
-    private double upServoVal, downServoVal;
-
+    private boolean hookUp;
+    
+    //private Vision vision;
+    //private int visionParam;
     public void controllerInit()
     {
         wheels = new Wheels(wheelPort1, wheelPort2, wheelPort3, wheelPort4);
         //controller1 = new XboxController(0);
-        //controller2 = new XboxController(1);
-        conveyor = new Conveyor(intConvPort1,intConvPort2);
-        //convDriveVal = ;
-        //intConvPort1 = ;
-        //intConvPort2 = ;
-        //drumPWMChannel = ;
-        //drumVal = ;
         //intakePort = ;
         //hookPort = ;
         //shooterVal = ;
         //colorPort = ;
         //colorWheelVal = ;
-        //driveVal = ;
-        //servoPWMChannel = ;
-        //upServoVal = ;
-        //downServoVal = ;
-      //  drawbridge = new DrawBridge(drumPWMChannel);
+        //visionParam = ;
+        //vision = new Vision(visionParam);
         intake = new Intake(intakePort);
-      //  hook = new HookExtension(hookPort);
+        //  hook = new HookExtension(hookPort);
         colorWheel = new ColorWheel(colorPort);
-       // colorArm = new ColorArm(servoPWMChannel);
+        // colorArm = new ColorArm(servoPWMChannel);
+        hookUp = true;
     }
-    //conveyer
-    public void Update(XboxController controller1, XboxController controller2) {
-        if (controller1.getAButtonPressed())
+    
+    public void update(XboxController controller1) {
+    if (controller1.getAButtonPressed())
     {
-        conveyor.drive(driveVal);
+
     }
     //drum in 
     if(controller1.getYButtonPressed())
     {
-        //drumVal = ;
-        //drawbridge.SetVal(drumVal);
+        wheels.inverse();
     }
     //drum out
     if(controller1.getBButtonPressed())
     {
-        //drumVal = ;
-        //drawbridge.SetVal(drumVal);
+        if(hookUp)
+            hook.lower();
+        else
+            hook.raise();        
     }
     
     //1st controller right bumper; hook up (that sounds weird)
     if(controller1.getBumperPressed(Hand.kRight))
     {
-        //hook.raise();
+        
     }
 
     //1st controller left bumper; hook down
     if(controller1.getBumperPressed(Hand.kLeft))
     {
-        //hook.lower();
+        intake.drive(intakeVal);
     }
     //inverse wheels
     if(controller1.getXButtonPressed())
     {
-        wheels.inverse();
+        //vision something
     }
-    //left trigger; intake
+    //left trigger
     if(controller1.getTriggerAxis(Hand.kLeft)>.1)
     {
-        intake.drive(intakeVal);
+        colorWheel.flyRun(colorWheelVal);
     }
     // right trigger; controls shooter
     if(controller1.getTriggerAxis(Hand.kRight)>.1)
     {
         shooter.shootRun(shooterVal);
     }
+
+
     //2nd controller 
     //servo up
+    /*
     if(controller2.getBButtonPressed())
     {
-       // colorArm.SetVal(upServoVal);
+       
     }
 
     if(controller2.getXButtonPressed())
@@ -116,7 +103,7 @@ public class Controller{
     //servo down
     if(controller2.getAButtonPressed())
     {
-    //    colorArm.SetVal(downServoVal);
+    
     }
 
     if(controller2.getYButton())
@@ -144,6 +131,7 @@ public class Controller{
     if(controller2.getTriggerAxis(Hand.kLeft)>.1)
     {
         colorWheel.flyRun(colorWheelVal);
+    }*/
     }
-    }
+    
 }
