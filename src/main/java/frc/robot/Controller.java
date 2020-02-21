b316583275490bfcd1ccc84188a11780a79399eb
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2020-02-21 15:33:03 -0800

    Revert "Delete Controller.java"
    This reverts commit 606944c10d77d27f5292be1c7bb3481a8f539be4.

---
+++ src/main/java/frc/robot/Controller.java
@@ -0,0 +1,115 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.SPI;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.AnalogInput;
+public class Controller{
+    // THE VALUES FOR THE DOUBLES BELOW NEED TO BE CONFIGURED MANUALLY
+
+    //main controller
+    private XboxController controller1;
+    private Wheels wheels;
+    private int intakeVal;
+    //    CONVEYOR CODE DELETED
+    private Intake intake;
+    private int intakePort;
+    private int wheelPort1, wheelPort2, wheelPort3, wheelPort4;
+    //private HookExtension hook;
+    private int hookPort;
+    private Shooter revShoot;
+    private Shooter shooter;
+    private int shooterVal;
+    private ColorWheel colorWheel;
+    private int colorPort;
+    private double colorWheelVal;
+    private AHRS ahrs;
+    private AnalogInput m_ultrasonic;
+    private boolean hookUp;
+    
+    //private Vision vision;
+    //private int visionParam;
+    public void controllerInit()
+    {
+        ahrs = new AHRS(SPI.Port.kMXP);
+        m_ultrasonic = new AnalogInput(0);
+        //wheelPort1 = wheelPort1;
+        //wheelPort2 = wheelPort2;
+        //wheelPort3 = wheelPort3;
+        //wheelPort4 = wheelPort4;
+        wheels = new Wheels(wheelPort1, wheelPort2, wheelPort3, wheelPort4);
+        controller1 = new XboxController(0);
+        //intakePort = ;
+        //hookPort = ;
+        //shooterVal = ;
+        //colorPort = ;
+        //colorWheelVal = ;
+        //visionParam = ;
+        //vision = new Vision(visionParam);
+        intake = new Intake(intakePort);
+        //  hook = new HookExtension(hookPort);
+        colorWheel = new ColorWheel(colorPort);
+        // colorArm = new ColorArm(servoPWMChannel);
+        hookUp = true;
+    }
+    
+    public void updateTeleop() {
+     // get values from the encoder. every 1 represents 1 rotation which is 2 pi r (6 pi) inches
+        double frontLeftRotations = wheels.getRotations("fL");
+        double backLeftRotations = wheels.getRotations("bR");
+
+    //get values from the navx gyro to see which angle we are facing
+    double angleFacing = ahrs.getAngle();
+
+    //get value from the ultrasonic sensor mounted in the front of the robot
+    double ultrasonicReading = getUltraSonicReading();
+
+    //logic code below
+    wheels.drive(controller1.getY(Hand.kLeft), controller1.getY(Hand.kRight));
+    if (controller1.getAButtonPressed())
+    {
+
+    }
+    //drum in 
+    if(controller1.getYButtonPressed())
+    {
+        wheels.inverse();
+    }
+  
+    //1st controller right bumper; hook up (that sounds weird)
+    if(controller1.getBumperPressed(Hand.kRight))
+    {
+        
+    }
+
+    //1st controller left bumper; hook down
+    if(controller1.getBumperPressed(Hand.kLeft))
+    {
+        intake.drive(intakeVal);
+    }
+    //inverse wheels
+    if(controller1.getXButtonPressed())
+    {
+        //vision something
+    }
+    //left trigger; revs up shooter
+    if(controller1.getTriggerAxis(Hand.kLeft)>.1)
+    {
+        revShoot.charge(0.6);
+    }
+    // right trigger; controls shooter
+    
+    if(controller1.getTriggerAxis(Hand.kRight)>.1 && controller1.getTriggerAxis(Hand.kLeft)>.1)
+    {
+        shooter.fire(); //kick fuel
+    }
+
+
+    }
+    private double getUltraSonicReading()
+    {
+        return m_ultrasonic.getValue()*0.125f/2.54f;
+    }
+    
+}
+
