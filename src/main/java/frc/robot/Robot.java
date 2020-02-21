2f4c5aa58f2684dbb0e7aa532d8b9c35a384e254
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2020-02-21 14:59:49 -0800

    added hand import to fix errors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
