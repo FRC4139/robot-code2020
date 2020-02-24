f3b0bca32646c57b8ae8bddac9f862e3b46f1632
pahels <pahel.srivastava@gmail.com>
2020-02-24 13:55:37 -0800

    color detection

---
+++ src/main/java/CV/colorDetection.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import math
+import time
+from nms import non_max_suppression_fast
+
+i = 0
+
+video = cv2.VideoCapture(0)
+
+# Downscale the image to a reasonable size to reduce compute
+scale = .5
+
+
+# Minimize false detects by eliminating contours less than a percentage of the image
+area_threshold = 0.01
+croppedPixels = 150
+
+while (True):
+
+	ret, orig_frame = video.read()
+	if not ret:
+		break
+	print(i)
+	i = i+1
+
+	#ropped = orig_frame[croppedPixels:, :]
+	#cv2.imshow("cropped", cropped)
+ 
+	orig_frame = cv2.resize(orig_frame, dim, interpolation = cv2.INTER_AREA)
+	frame = cv2.GaussianBlur(orig_frame, (5, 5), 0)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	
+	mask0 = cv2.inRange(hsv,(67, 51, 0), (112, 114, 255) )
+	
