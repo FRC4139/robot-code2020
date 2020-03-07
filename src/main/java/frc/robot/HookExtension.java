986ccbb04d6050ea988bacf1dca1b18da26e1574
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2020-03-07 12:05:24 -0800

    changed talons to victors in hook ext

--- src/main/java/frc/robot/HookExtension.java
+++ src/main/java/frc/robot/HookExtension.java
@@ -1,16 +1,17 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class HookExtension
 {
-    private WPI_TalonSRX motor1;
-    private WPI_TalonSRX motor2;
+    private WPI_VictorSPX motor1;
+    private WPI_VictorSPX motor2;
     //port 1 place holder
     public HookExtension(int port1, int port2)
      {
-        motor1 = new WPI_TalonSRX(port1);
-        motor2 = new WPI_TalonSRX(port2);
+        motor1 = new WPI_VictorSPX(port1);
+        motor2 = new WPI_VictorSPX(port2);
     }
     
     //lowers hook
