1ddc1e448fb75265838e44daad422a367ea6d718
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2020-03-11 12:15:14 -0700

    moves at 0.6 instead of 0.5 (drive wheels)

--- src/main/java/frc/robot/Wheels.java
+++ src/main/java/frc/robot/Wheels.java
@@ -50,8 +50,8 @@ public class Wheels {
     // Negative speed turns wheels backwards
     public void drive(double leftSpeed, double rightSpeed) {
 
-        leftSpeed *= 0.5;
-        rightSpeed *= 0.5;
+        leftSpeed *= 0.6;
+        rightSpeed *= 0.6;
         if(inverseState)
         {
             //if inversState is true, reverse the speeds and call drive 
